d98ed6348a0e10abf35aaeb5462ac9149f4c2dc0
Susie6 <201830582210@mail.scut.edu.cn>
2021-05-14 14:29:38 +0800

    rr

---
+++ client/js/main.js
@@ -0,0 +1,6 @@
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(ElementUI)
+
--- js/login.js
+++
@@ -1,6 +0,0 @@
-// var app = new Vue({
-//     el:"#app",
-//     data:{
-//         msg:"学习vue中"
-//     }
-// })
