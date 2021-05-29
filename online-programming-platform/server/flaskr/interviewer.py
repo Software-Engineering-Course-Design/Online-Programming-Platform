f23bd2377153036ecf96a913893039db3f4a8623
kelly-source <15889267686@163.com>
2021-05-29 12:47:23 +0800

    usedtime

--- online-programming-platform/server/flaskr/interviewer.py
+++ online-programming-platform/server/flaskr/interviewer.py
@@ -3,6 +3,7 @@ from . import db
 from flask_cors import *
 import sqlite3
 import json
+from datetime import datetime
 
 interviewer = Blueprint('interviewer', __name__)
 
@@ -156,30 +157,57 @@ def initial_interview():
         applicant = request.json.get("applicant")  # 数组
         questionNumber = request.json.get("questionNumber")
         questionID = request.json.get("questionID")  # 数组
-        print(len(questionID))
         createWay = request.json.get("createWay")
+        startTime = request.json.get("startTime")
+        endTime = request.json.get("endTime")
         query = "SELECT max(sessionID) FROM interview"
         # 通过用户的id来查询用户资料（one=True返回第一条数据）
         result = query_db(query, one=True)
         sessionID = tuple(result)[0]
         status = "False"  # 面试状态初始为未参加状态
 
+        startTime = datetime.strptime(startTime, '%Y-%m-%d %H:%M:%S')
+        endTime = datetime.strptime(endTime, '%Y-%m-%d %H:%M:%S')
+        # 计算用时
+        timeUsed = (endTime - startTime).total_seconds()
+
+        m, s = divmod(timeUsed, 60)
+        h, m = divmod(m, 60)
+        timeUsed = "%02d:%02d:%02d" % (h, m, s)
+
         if sessionID is None:
             sessionID = 0
         sessionID += 1
+        for k in range(len(applicant)):
+            query = "SELECT id FROM user WHERE username='{}'".format(applicant[k])
+            query = query_db(query, one=True)
+            if query is None:
+                return dict(ifSuccess=False, msg="邀请失败，邀请用户不存在")
         if len(questionID) == questionNumber:
             connection = db.get_db()
             for i in range(questionNumber):
                 for j in range(len(applicant)):
-                    query = "INSERT INTO interview(sessionID,username,applicant, questionNumber, questionID, createWay, status) values('{}','{}','{}','{}','{}','{}','{}')"\
-                        .format(sessionID, username, applicant[j], questionNumber, questionID[i], createWay, status)
+                    query = "INSERT INTO interview(sessionID,username,applicant, questionNumber, questionID, createWay, status, startTime, endTime, timeUsed) " \
+                            "values('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')"\
+                        .format(sessionID, username, applicant[j], questionNumber, questionID[i], createWay, status, startTime, endTime, timeUsed)
                     connection.execute(query)
                     connection.commit()
-            return dict(ifSuccess=True, msg="新建成功")
+            return dict(ifSuccess=True, msg="邀请成功", sessionID=sessionID)
         else:
-            return dict(ifSuccess=False, msg="新建失败，面试题错误")
+            return dict(ifSuccess=False, msg="邀请失败，面试题错误")
     else:
-        return dict(ifSuccess=False, msg="新建失败，网络发生故障")
+        return dict(ifSuccess=False, msg="邀请失败，网络发生故障")
+
+
+'''{
+    "username":"a",
+    "applicant":["d","b"],
+    "questionNumber":2,
+    "questionID":[1,2],
+    "createWay":"true",
+    "startTime":"2004-01-01 02:34:56",
+    "endTime":"2004-01-01 03:34:56"
+}'''
 
 
 # 面试官批改提交代码
@@ -200,15 +228,15 @@ def check_code():
                 dict2 = answer[j]
                 questionID = dict2["idx"]
                 result = dict2["value"]
-                query = "SELECT result FROM code WHERE username='{}' AND questionID='{}'".format(username, questionID)
+                query = "SELECT result FROM code WHERE username='{}' AND questionID='{}' AND sessionID='{}'".format(username, questionID, sessionID)
                 result_before = query_db(query, one=True)
                 result_before = tuple(result_before)
                 print('3', result_before[0])
                 if result_before[0] == '0':
                     # query = "INSERT INTO code(result,username,questionID) values('{}','{}','{}')"\
                      # .format(result, username, questionID)
-                    query = "UPDATE code SET result='{}' WHERE username='{}' AND questionID='{}'" \
-                        .format(result, username, questionID)
+                    query = "UPDATE code SET result='{}' WHERE username='{}' AND questionID='{}' AND sessionID='{}'" \
+                        .format(result, username, questionID, sessionID)
                     connection.execute(query)
                     connection.commit()
                     print('success')
